a983609a7fa4029868a5071dea675e79b1c0ad3f
MahamBasharat99 <mahambasharat8@gmail.com>
2020-06-14 14:10:52 -0700

    javascript assignment5 GIT

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+// Assignment 3
+// task#1
+var age;
+age= 21;
+alert("I am " + age +" years old");
+
+// task#2
+var sum;
+sum="";
+sum++;
+alert("You have visted this web page" + sum +" times.");
+
+// task#3
+var birthYear;
+birthYear=+(1999);
+document.write("<br> My Birthyear is  " +(+birthYear));
+document.write(" <br> Data Type of my variable is number");
+
+
